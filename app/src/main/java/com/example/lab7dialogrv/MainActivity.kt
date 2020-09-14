1a60e0dddf4a54429e98fddc5aeb6bd7719f1ad3
pukjira1999 <69905995+pukjira1999@users.noreply.github.com>
2020-09-14 16:52:14 +0700

    ส่ง Lab7DialogRV_Sec1_613020169-5

---
+++ app/src/main/java/com/example/lab7dialogrv/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.lab7dialogrv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.add_dialog_layout.*
+import layout.Student
+import layout.StudentsAdapter
+
+class MainActivity : AppCompatActivity() {
+    val studentList : ArrayList<Student> = arrayListOf<Student>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        studenData()
+        recycler_view.adapter = StudentsAdapter(this.studentList,applicationContext)
+        recycler_view.layoutManager = LinearLayoutManager(applicationContext)
+    }
+    fun studenData(){
+        studentList.add(Student("621234567-5", "Alice" , 20))
+        studentList.add(Student("629876543-2" , "Bob" , 2))
+    }
+    fun addStudent(view: View){
+        val mDialogView : View = LayoutInflater.from(this).inflate(R.layout.add_dialog_layout, null)
+        val myBuilder = AlertDialog.Builder(this)
+        myBuilder.setView(mDialogView)
+
+        val mAlertDialog : AlertDialog = myBuilder.show()
+        mAlertDialog.btnAdd.setOnClickListener {
+        studentList.add(
+            Student(
+                mAlertDialog.edt_id.text.toString(),
+                mAlertDialog.edt_name.text.toString(),
+                mAlertDialog.edt_age.text.toString().toInt()
+            )
+        )
+       recycler_view.adapter?.notifyDataSetChanged()
+            Toast.makeText(
+                applicationContext,
+                "This student is added successfully",
+                Toast.LENGTH_LONG
+            ).show()
+            mAlertDialog.dismiss()
+        }
+        mAlertDialog.btnCancel.setOnClickListener {
+            mAlertDialog.dismiss()
+        }
+    }
+}
